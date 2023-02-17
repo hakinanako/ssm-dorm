de927d681a9b92e103016673bf54340a0b2cb81a
hakiko <hkk200493719@gmail.com>
2023-02-17 23:54:35 +0800

    push test commit

--- src/main/java/com/hkk/ssm/Main.java
+++ src/main/java/com/hkk/ssm/Main.java
@@ -2,6 +2,6 @@ package com.hkk.ssm;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hi world!");
     }
 }

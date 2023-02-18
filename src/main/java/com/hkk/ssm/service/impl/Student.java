69eb05a610d2fd5d28dcfe02049b469ed99d7756
hakiko <hkk200493719@gmail.com>
2023-02-19 00:04:27 +0800

    ee

---
+++ src/main/java/com/hkk/ssm/service/impl/Student.java
@@ -0,0 +1,8 @@
+package com.hkk.ssm.service.impl;
+
+import com.hkk.ssm.service.StudentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Student implements StudentService {
+}

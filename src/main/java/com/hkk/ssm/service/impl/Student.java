2ba8d26e4fc7170315b04c5f67b4398d2e398872
hakiko <hkk200493719@gmail.com>
2023-02-19 23:05:10 +0800

    mybatis~~~

--- src/main/java/com/hkk/ssm/service/impl/Student.java
+++
@@ -1,8 +0,0 @@
-package com.hkk.ssm.service.impl;
-
-import com.hkk.ssm.service.StudentService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class Student implements StudentService {
-}
---
+++ src/main/java/com/hkk/ssm/service/impl/StudentServiceImpl.java
@@ -0,0 +1,13 @@
+package com.hkk.ssm.service.impl;
+
+import com.hkk.ssm.service.StudentService;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+//    @Autowired
+//    private SqlSessionFactory sqlSessionFactory;
+}
